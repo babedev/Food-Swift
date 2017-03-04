1f85eebb5a3b068067ec198d3164f97bc6f56106
Kazuki Yamamoto <kazuki.ccho@gmail.com>
2017-03-04 14:28:50 +0900

    wip

---
+++ Food-Swift-iOS/FoodSwift/FoodSwift/CustomKolodaView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomKolodaView.swift
+//  FoodSwift
+//
+//  Created by Kazuki Yamamoto on 2017/03/04.
+//  Copyright © 2017年 tryswift. All rights reserved.
+//
+
+import UIKit
+
+class CustomKolodaView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Food-Swift-iOS/FoodSwift/FoodSwift/Food.swift
@@ -0,0 +1,9 @@
+//
+//  Food.swift
+//  FoodSwift
+//
+//  Created by Kazuki Yamamoto on 2017/03/04.
+//  Copyright © 2017年 tryswift. All rights reserved.
+//
+
+import Foundation
